ed1071cae14c284895da800b99a80deede45a038
Cameron Burns <burnscam@oregonstate.edu>
2019-01-28 15:40:40 -0800

    added ALU

--- 01/logic_gate.py
+++ 01/logic_gate.py
@@ -1,6 +1,4 @@
 i = 0
 while i < 16:
-	print("Mux(a=a[" +str(i)+ "], b=b[" +str(i)+ "], sel=sel[0], out=out1);");
-	print("Mux(a=c[" +str(i)+ "], b=d[" +str(i)+ "], sel=sel[0], out=out2);");
-	print("Mux(a=out1, b=out2, sel=sel[1], out=out);");
+	print("FullAdder(a=a[" +str(i)+ "], b=b[" +str(i)+ "], c=carry"+str(i)+", sum=out[" +str(i)+ "], carry=carry"+str(i)+";");
 	i += 1
