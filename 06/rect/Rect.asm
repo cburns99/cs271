779d57a1b8582295a22472ed643a7b80ab8f34e7
Cameron Burns <burnscam@oregonstate.edu>
2019-03-08 19:44:35 -0800

    passed Rect

--- 06/rect/Rect.asm
+++ 06/rect/Rect.asm
@@ -1,7 +1,7 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/06/rect/Rect.asm
+// File name: projects/06/rect/Rect.asmgit
 // Draws a rectangle at the top-left corner of the screen.
 // The rectangle is 16 pixels wide and R0 pixels high.
 
